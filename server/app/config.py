3cb44af628e8de2f741a673013b024753eb9b6e4
CaioLr <56622666+CaioLr@users.noreply.github.com>
2025-01-04 23:49:49 -0300

    fix(config) Removing unecessary code

--- server/app/config.py
+++ server/app/config.py
@@ -6,7 +6,3 @@ load_dotenv()
 
 class AppConfig:
     SECRET_KEY=os.environ["SECRET_KEY"]
-    SESSION_TYPE = "redis"
-    SESSION_PERMANENT = False
-    SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
